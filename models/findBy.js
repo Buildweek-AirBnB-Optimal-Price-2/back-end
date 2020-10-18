1417a29a175bdb8624239a82427607f019b35014
sam-h-hall <sam_h_hall@icloud.com>
2020-10-18 09:54:39 -0500

    Get route returns almost anything I want in my db

--- models/findBy.js
+++ models/findBy.js
@@ -1,10 +1,19 @@
 const db = require("../database/dbConfig");
 
-module.exports = async (tableString, filter) => {
+module.exports = async (tableString, keyString, value) => {
   try {
-    return db(tableString).where(filter).first();
+    return db(tableString).where(keyString, ">=" ,value);
   } catch (err) {
     console.log(err);
     return false;
-  }
-}
+  };
+};
+
+// module.exports = async (tableString, filter) => {
+//   try {
+//     return db(tableString).where(filter).first();
+//   } catch (err) {
+//     console.log(err);
+//     return false;
+//   }
+// }
--- routers/authorized_routes.js
+++ routers/authorized_routes.js
@@ -1,20 +1,27 @@
 const router = require("express").Router()
-const { find, findById, } = require("../models/index");
-// const verify_token = require("../middleware/verify_token");
+const { find, findById, findBy } = require("../models/index");
+const verify_token = require("../middleware/verify_token");
 
 // how do I add a query to search for addresses of properties?
-router.get("/:table/:id?", /*verify_token, */ async (req, res, next) => {
-  const { table, id } = req.params;
+// rn I can't think of a use for searching by table and keyName --> UNLESS keyname(key_OR_id) could also be an id
+router.get("/:table/:key_OR_id?/:value?", /*verify_token, */ async (req, res, next) => {
+  
+  const {table, key_OR_id, value} = req.params;
     
   try {
-    let user;
     
-    id ?
-      user = await findById(table.toString(), id)
-    : user = await find(table.toString());
+    let tableData;
 
-    // if res.status defaults to 200 on a get req, do I really need to add it manually?
-    res.send(user);
+    if (!key_OR_id && !value) {
+      tableData = await find(table.toString())
+    } else if (!value) {
+      tableData = await findById(table.toString(), key_OR_id);
+    } else {
+      tableData = await findBy(table.toString(), key_OR_id.toString(), value);
+    };
+    
+    res.send(tableData);
+
   } catch (err) {
     console.log(err);
     res.status(500).json({
@@ -24,3 +31,18 @@ router.get("/:table/:id?", /*verify_token, */ async (req, res, next) => {
 });
 
 module.exports = router;
+
+
+    // const { table, id } = req.params;
+
+    // let user;
+
+    // tableData = await findBy(table.toString(), keyName.toString(), value);
+
+    // id ?
+    // console.log(id)
+    //   // user = await findById(table.toString(), id)
+    // : user = await find(table.toString());
+
+    // if res.status defaults to 200 on a get req, do I really need to add it manually?
+    // res.send(user);
